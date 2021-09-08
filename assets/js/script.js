341fd3996ffc07d9ae57e7f3b09a7aaa18f7c0c4
nguyen-william93 <=>
2021-09-08 00:04:01 -0500

    add comment for reader

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,6 @@
-
 var saveData = function(task, hour){
-    //load pre-existing data;
     var userData = JSON.parse(localStorage.getItem("schedule")) || []
+
     if (task !== ""){
         userObj = {
             task: task,
@@ -10,13 +9,12 @@ var saveData = function(task, hour){
         userData.push(userObj);
         window.localStorage.setItem("schedule", JSON.stringify(userData))
     }
-    
-}
+};
 
 var loadData = function(){
     var tasks = JSON.parse(localStorage.getItem("schedule"));
 
-    //looping over each time zone
+    //looping over each object in the list
     $.each(tasks, function(list, arr){
         displaySavedTask(arr.task, arr.hour);
     })
@@ -27,26 +25,53 @@ var displaySavedTask = function(task, hour){
     $(".hour").each(function(){
         if($(this).text() === hour){
             $(this).siblings("textarea").text(task);
+        } else {
+            $(this).siblings("textarea").text();
+        }
+    })
+};
+
+//continuously checking the time against our time block
+var checkTime = function(){
+    var currentTime = moment().hour(); //using momemt.js to obtain military time. 
+    $(".hour").each(function(){
+        var timeBlock = parseInt($(this).text());
+        if (currentTime > timeBlock){
+            $(this).siblings("textarea").addClass("past");
+        } else if (currentTime < timeBlock){
+            $(this).siblings("textarea").addClass("future");
+        } else {
+            $(this).siblings("textarea").addClass("present");
         }
     })
 }
 
-//running the function on the page
-//set interval to update the clock every second
-//load the any save data
-//logic for the save icon/button
+//calling function to run the opage
 $(document).ready(function(){
+
+    //using momemt to get current time and day
+    //interval to update current time every second
+    var currentDay = $("#currentDay").text(moment().format("MMMM DD YYYY, h:mm:ss a")); 
     var interval = setInterval(function(){
-        $("#currentDay").text(moment().format("MMMM DD YYYY, h:mm:ss a"));
+        currentDay;
     },1000);
 
+    //check the time and add the correct color and display the screen base on any saved data
+    checkTime();
     loadData();
 
+    //save to local data when button is clicked
     $(".saveBtn").on("click", function(){
         var userInput = $(this).siblings("textarea").val().trim();
         var hourSpan = $(this).siblings(".hour").text().trim();
         saveData(userInput, hourSpan)
     })
-})
+
+    //clear the local data and refresh the page
+    $("#clear-task").on("click", function(){
+        localStorage.clear();
+        location.reload();
+    })
+});
 
 
